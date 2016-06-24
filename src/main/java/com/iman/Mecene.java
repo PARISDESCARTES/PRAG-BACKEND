2acbc69c04fba378c7f7ed96f0848a4a79bec0da
Alex <alexandre.rosselin@gmail.com>
2016-06-24 11:44:42 +0200

    ADD MECENE CLASS

---
+++ src/main/java/com/iman/Mecene.java
@@ -0,0 +1,49 @@
+package com.iman;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Date;
+
+
+@Entity
+@Data
+@Table(name = "mecene")
+public class Mecene {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="idmecene")
+    private long idMecene;
+
+    @Column(name="adressemecene")
+    private String adresseMecene;
+
+    @Column(name="siretmecene")
+    private String siretMecene;
+
+    @Column(name="nommecene")
+    private String nomMecene;
+
+    @Column(name="codepostalmecene")
+    private String codePostalMecene;
+
+    @Column(name="villemecene")
+    private String villeMecene;
+
+    @Column(name="paysmecene")
+    private String paysMecene;
+
+    @Column(name="telephoneaccueil")
+    private String telephoneAccueil;
+
+    @Column(name="idcompte")
+    private long idCompte;
+
+    @Column(name="idformejuridique")
+    private long idFormeJuridique;
+
+    @Column(name="idtypepartenaire")
+    private long idTypePartenaire;
+
+}
