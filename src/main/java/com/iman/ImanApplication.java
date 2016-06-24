9af6e4e96c46f1087db1f7c51daca136fa31164e
Sébastien CHEN <Sbastien CHEN>
2016-06-24 10:51:39 +0200

    init back end

---
+++ src/main/java/com/iman/ImanApplication.java
@@ -0,0 +1,12 @@
+package com.iman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImanApplication.class, args);
+	}
+}
