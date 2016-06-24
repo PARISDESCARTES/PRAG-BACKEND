0edecd27fded1807bbff33085061f2970cc34f7b
Alex <alexandre.rosselin@gmail.com>
2016-06-24 13:49:48 +0200

    ADD CLASSES DAO

---
+++ src/main/java/com/iman/ModePaiement.java
@@ -0,0 +1,25 @@
+package com.iman;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Date;
+
+
+@Entity
+@Data
+@Table(name = "modepaiement")
+public class ModePaiement {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="idmodepaiement")
+    private long idModePaiement;
+
+
+    @Column(name="nommodepaiement")
+    private String nomModePaiement;
+
+
+
+}
