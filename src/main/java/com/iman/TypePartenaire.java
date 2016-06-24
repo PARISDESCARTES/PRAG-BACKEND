0b7752e4e183d58510039563781fe73cdebaa178
Alex <alexandre.rosselin@gmail.com>
2016-06-24 11:49:23 +0200

    ADD TYPEPARTENAIRE CLASS

---
+++ src/main/java/com/iman/TypePartenaire.java
@@ -0,0 +1,23 @@
+package com.iman;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Date;
+
+
+@Entity
+@Data
+@Table(name = "typepartenaire")
+public class TypePartenaire {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="idtypepartenaire")
+    private long idTypePartenaire;
+
+    @Column(name="typePartenaire")
+    private String idEvent;
+
+
+}
