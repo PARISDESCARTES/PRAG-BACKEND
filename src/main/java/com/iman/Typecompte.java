37c18e5c0271565454cc8d41229c4251f9612497
Alex <alexandre.rosselin@gmail.com>
2016-06-24 12:12:27 +0200

    ADD CLASSES DAO

--- src/main/java/com/iman/Typecompte.java
+++ src/main/java/com/iman/Typecompte.java
@@ -3,20 +3,32 @@ package com.iman;
 import lombok.Data;
 
 import javax.persistence.*;
+import java.util.Date;
 
 
 @Entity
 @Data
 @Table(name = "typecompte")
-public class Typecompte {
+public class TypeCompte {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name="idtypecompte")
-    private long idTypeCompte;
+    @Column(name="idenfant")
+    private long idEnfant;
 
-    @Column(name="nomtypecompte")
-    private String nomTypeCompte;
+    @Column(name="datenaissanceenfant")
+    private Date dateNaissanceEnfant;
 
+    @Column(name="sexeenfant")
+    private char sexeEnfant;
+
+    @Column(name="diagnosticenfant")
+    private String diagnosticEnfant;
+
+    @Column(name="descriptionenfant")
+    private String descriptionEnfant;
+
+    @Column(name="idcompte")
+    private long idCompte;
 
 }
